8e2ff1b90ba5215e618e1e46d1050d48dc8d3c95
gab <gmawong10@gmail.com>
2025-08-05 03:39:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="o

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -78,9 +78,10 @@ export default {
         },
       },
       borderRadius: {
+        xl: 'calc(var(--radius) + 4px)',
         lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+        md: 'calc(var(--radius) - 4px)',
+        sm: 'calc(var(--radius) - 8px)',
       },
       keyframes: {
         'accordion-down': {
